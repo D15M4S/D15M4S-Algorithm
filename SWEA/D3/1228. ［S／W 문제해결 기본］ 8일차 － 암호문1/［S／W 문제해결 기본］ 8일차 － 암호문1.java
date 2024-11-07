4abdf38c3e397e0002b1926a460ace1cc1f12062
박종범 <122260287+D15M4S@users.noreply.github.com>
2024-11-07 22:09:25 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문1, Time: 107 ms, Memory: 21,548 KB -BaekjoonHub

---
+++ SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Solution {
+	
+    public static void main(String args[]) throws Exception {
+        
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	//Test_case
+    	for(int test_case = 1; test_case <= 10; test_case++) {
+    		
+    		int N, M;
+    		N = Integer.parseInt(br.readLine());
+    		
+    		List<Integer> list = new LinkedList<>();
+    		
+    		// 원본 암호문 입력
+    		StringTokenizer st = new StringTokenizer(br.readLine());
+    		for(int n = 0; n < N; n++) {
+    			list.add(Integer.parseInt(st.nextToken()));
+    		}
+    		
+    		// 명령어의 개수
+    		M = Integer.parseInt(br.readLine());
+    		
+    		// 명령어
+    		StringTokenizer st2 = new StringTokenizer(br.readLine());
+    		for(int m = 0; m < M; m++) {
+    			st2.nextToken();	//I
+    			int idx = Integer.parseInt(st2.nextToken());
+    			int num = Integer.parseInt(st2.nextToken());
+    			for(int i = 0; i < num; i++) {
+    				list.add(idx + i, Integer.parseInt(st2.nextToken()));
+    			}
+    		}
+    		// 출력
+    		
+    		System.out.print("#" + test_case);
+    		for(int i = 0; i < 10; i++) {
+    			System.out.print(" " + list.get(i));
+    		}
+    		System.out.println();
+    	}
+    }
+}
